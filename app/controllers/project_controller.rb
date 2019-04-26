09608b741416e1b4b2de5ffdf60c27a456512ccb
hillevieklow <hillevieklow@gmail.com>
2019-04-26 19:23:28 -0400

    init project controller

--- app/controllers/project_controller.rb
+++ app/controllers/project_controller.rb
@@ -1,2 +1,23 @@
 class ProjectController < ApplicationController
+	def all
+    projects = Project.all
+
+    render json: projects
+  end
+
+  def new
+    puts params
+    project = Project.create(
+      description: params[:name],
+      deadline: params[:deadline]
+    )
+
+    render json: project
+  end
+
+  def get
+    project = Project.find(params[:id])
+
+    render json: project
+  end
 end
