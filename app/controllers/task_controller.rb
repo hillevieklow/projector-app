d27e06ad85e776771a19884826ae875223e1e1a6
hillevieklow <hillevieklow@gmail.com>
2019-04-26 19:24:32 -0400

    init task controller

--- app/controllers/task_controller.rb
+++ app/controllers/task_controller.rb
@@ -1,2 +1,21 @@
 class TaskController < ApplicationController
+	def new
+    newTask = Task.create(
+      name: params[:name],
+      done: false,
+      project_id: params[:projectId]
+    )
+
+    render json: newTask
+  end
+
+  def complete
+    task = Task.find(params[:id])
+    unless task.nil?
+      task.done = params[:done]
+      task.save
+    end
+
+    render json: task
+  end
 end
