dbf88cbac2b6255242e3cf82a6c84378cc8ca455
hillevieklow <hillevieklow@gmail.com>
2019-04-26 19:20:41 -0400

    adds table columns

--- db/migrate/20190426231206_create_projects.rb
+++ db/migrate/20190426231206_create_projects.rb
@@ -1,7 +1,9 @@
 class CreateProjects < ActiveRecord::Migration[5.2]
   def change
     create_table :projects do |t|
-
+      t.string :description
+      t.datetime :deadline
+      
       t.timestamps
     end
   end
--- db/migrate/20190426231218_create_tasks.rb
+++ db/migrate/20190426231218_create_tasks.rb
@@ -1,6 +1,9 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
+      t.string :name
+      t.boolean :done
+      t.integer :project_id
 
       t.timestamps
     end
