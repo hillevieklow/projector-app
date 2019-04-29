5d9d4f42c5d0c6331937db6869bd5033ce1ceb2c
hillevieklow <hillevieklow@gmail.com>
2019-04-28 20:52:10 -0400

    create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import store from './store';
+
+import ProjectListContainer from './containers/ProjectList';
+import ProjectContainer from './containers/Project';
+import NewProjectContainer from './containers/NewProject';
+
+import styles from './App.module.css';
+import {Container, Header} from "semantic-ui-react";
+
+class App extends Component {
+  render() {
+    return (
+        <Provider store={store}>
+          <Container>
+            <Header className={styles.header}>
+              Projector -- simple project management
+            </Header>
+            <Router>
+              <Route exact path="/" component={ProjectListContainer} />
+              <Route exact path="/new-project" component={NewProjectContainer} />
+              <Route path="/project/:id" component={ProjectContainer} />
+            </Router>
+          </Container>
+        </Provider>
+    );
+  }
 }
-
 export default App;
